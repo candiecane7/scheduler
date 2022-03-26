31428bcd9f820d9de74a67f7fd63f97852a19cbe
Candice Hall <hall.candice@outlook.com>
2022-03-26 17:17:02 -0400

    everything but sizing works

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -4,40 +4,48 @@
 // functions needed:
 // save tasks
 //load tasks
-//clear all tasks
 var dateAndTime = document.querySelector("#currentDay");
 
 //display current date at the top of the calendar
-var refreshTime = function(){
+var refreshTime = function () {
     // dateAndTime.textContent = "";
     var currentTime = moment();
     dateAndTime.textContent = currentTime.format("MMM Do, YYYY - hh:mm a");
 }
-setInterval(refreshTime, 1000);
 
-var hour = document.querySelectorAll(".row")
-hour.forEach((hour) => {
-    time = $(hour).attr("id");
-    currentTime = moment().hours();
-    var colorSet = $(hour).children("textarea")
+var setColors = function () {
+    var hour = document.querySelectorAll(".row")
+    hour.forEach((hour) => {
+        time = $(hour).attr("id");
+        currentTime = moment().hours();
+        var colorSet = $(hour).children("textarea")
 
-    if (time > currentTime){
-        $(colorSet).removeClass("present");
-        $(colorSet).removeClass("past");
-        $(colorSet).addClass("future");
-    }
-    else if (time < currentTime){
-        $(colorSet).removeClass("present");
-        $(colorSet).removeClass("future");
-        $(colorSet).addClass("past");
-    } else {
-        $(colorSet).removeClass("future");
-        $(colorSet).removeClass("past");
-        $(colorSet).addClass("present");
-    }
-})
+        if (time > currentTime) {
+            $(colorSet).removeClass("present");
+            $(colorSet).removeClass("past");
+            $(colorSet).addClass("future");
+        }
+        else if (time < currentTime) {
+            $(colorSet).removeClass("present");
+            $(colorSet).removeClass("future");
+            $(colorSet).addClass("past");
+        } else {
+            $(colorSet).removeClass("future");
+            $(colorSet).removeClass("past");
+            $(colorSet).addClass("present");
+        }
+    })
+};
 
-var loadTasks = function(){
+refreshTime();
+setColors();
+
+setInterval(function(){
+    refreshTime();
+    setColors();
+}, 1000);
+
+var loadTasks = function () {
     taskContainer = document.querySelectorAll(".row");
 
     taskContainer.forEach((taskContainer) => {
@@ -48,12 +56,12 @@ var loadTasks = function(){
 
         $(taskText).text(task);
     });
-    
-   
+
 }
 loadTasks();
 
-var saveTask = function(){
+var saveTask = function () {
+    console.log(this);
     var taskText = $(this).prev().val();
     var taskHour = $(this).parent().attr("id");
 
@@ -61,4 +69,4 @@ var saveTask = function(){
 
 }
 
-$("#save-task").on("click", saveTask);
+$(".saveBtn").on("click", saveTask);
