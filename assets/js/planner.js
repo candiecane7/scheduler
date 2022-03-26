be0e1b4f26c091dbc8805ea4d64808ad18397843
Candice Hall <hall.candice@outlook.com>
2022-03-26 12:30:04 -0400

    captured text so far

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -5,7 +5,7 @@
 // save tasks(in the cards)
 //edit tasks
 //load tasks
-//
+//clear all tasks
 var dateAndTime = document.querySelector("#currentDay");
 
 //display current date at the top of the calendar
@@ -16,3 +16,24 @@ var refreshTime = function(){
 }
 setInterval(refreshTime, 1000);
 //create cards for each hour of the day - 7-9
+
+// var editTask = function(){
+//      var text = $(this)
+//     .text()
+//     .trim();
+
+//   var textInput = $("<textarea>")
+//   .addClass("text-center col-12 col-lg-10 border")
+//   .val(text);
+
+//   $(this).replaceWith(textInput);
+//  }
+
+var saveTask = function(){
+    var taskText = $(this).prev().val();
+    var taskHour = $(taskText).prevUntil(".time");
+    console.log(taskHour);
+}
+
+// $("#text-container").on("click", editTask);
+$("#save-task").on("click", saveTask);
